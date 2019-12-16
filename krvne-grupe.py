6a5d9e3d3cf8817b49139211859c5d968494cf79
Ivan Rakarić <rakarich.ivan@gmail.com>
2019-12-16 14:06:55 +0100

    krvne grupe

---
+++ krvne-grupe.py
@@ -0,0 +1,22 @@
+d = input("Krvna grupa donora: ")
+r1 = input("Rh faktor donora: ")
+p = input("Krvna grupa primatelja: ")
+r2 = input("Rh faktor primatelja: ")
+x = "Dopušteno"
+y = "Nedopušteno"
+
+if d == p and r1 == r2:
+    print(x)
+elif d == p and r1 == "-" and r2 == "+":
+    print(x)
+elif d == "0" and p == "b" or p == "a" or p == "ab" and r1 == r2:
+    print(x)
+elif d == "0" and p == "b" or p == "a" or p == "ab" and r1 == "-" and r2 == "+":
+    print(x)
+elif p == "ab" and d == "a" or d == "b" and r1 == r2:
+    print(x)
+elif p == "ab" and d == "a" or d == "b" and r1 == "-" and r2 == "+":
+    print(x)
+else:
+    print(y)
+
