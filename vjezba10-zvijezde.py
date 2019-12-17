578b919352af9a9642f06247a86179686f6817c4
Ivan Rakarić <rakarich.ivan@gmail.com>
2019-12-17 18:56:15 +0100

    stavio range stupaca od 0 i sad radi

--- vjezba10-zvijezde.py
+++ vjezba10-zvijezde.py
@@ -2,6 +2,6 @@ a = int(input("Broj redaka: "))
 brojac = "*"
 
 for brojredaka in range(1, a + 1):
-    for brojstupaca in range(1, brojredaka):
+    for brojstupaca in range(0, brojredaka):
         print(brojac, end="")
     print()
