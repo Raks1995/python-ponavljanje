38b0bc8214d91a67f869b65785020854ee67f6fa
Ivan Rakarić <rakarich.ivan@gmail.com>
2019-12-17 18:48:17 +0100

    stavio zagragde i sad radi

--- krvne-grupe.py
+++ krvne-grupe.py
@@ -2,21 +2,16 @@ d = input("Krvna grupa donora: ")
 r1 = input("Rh faktor donora: ")
 p = input("Krvna grupa primatelja: ")
 r2 = input("Rh faktor primatelja: ")
-x = "Dopušteno"
-y = "Nedopušteno"
+x = "Transfuzija dopuštena."
+y = "Transfuzija nije dopuštena."
 
 if d == p and r1 == r2:
     print(x)
 elif d == p and r1 == "-" and r2 == "+":
     print(x)
-elif d == "0" and p == "a" or p == "b" or p == "c" and r1 == r2:
+elif d == "0" and (p == "a" or p == "b" or p == "c") and (r1 == r2 or r1 == "-" and r2 == "+"):
     print(x)
-elif d == "0" and p == "a" or p == "b" or p == "c" and r1 == "-" and r2 == "+":
-    print(x)
-elif p == "c" and d == "a" or d == "b" and r1 == r2:
-    print(x)
-elif p == "c" and d == "a" or d == "b" and r1 == "-" and r2 == "+":
+elif p == "c" and (d == "a" or d == "b") and (r1 == r2 or r1 == "-" and r2 == "+"):
     print(x)
 else:
     print(y)
-
